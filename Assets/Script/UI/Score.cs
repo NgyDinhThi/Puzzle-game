28fafa5146b7c822af0c93bfde52a87dd61b20d1
NgDinhThi <dinhthit25@gmail.com>
2025-08-04 17:13:50 +0700

    tạo save file
    để lưu trữ điểm cao nhất

--- Assets/Script/UI/Score.cs
+++ Assets/Script/UI/Score.cs
@@ -1,39 +1,78 @@
-using Unity.VisualScripting;
+﻿using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections;
+
+[System.Serializable]
+public class BestScoreData
+{
+    public int score = 0;
+}
 
 public class Score : MonoBehaviour
 {
     public TMP_Text scoreText;
+    private bool newBestScore =false;
+    private BestScoreData bestScore_ = new BestScoreData();
 
     private int currentScores_;
+    private string bestScoreKey_ = "bskdata";
+    private void Awake()
+    {
+        if (BinaryDataStream.Exist(bestScoreKey_))
+        {
+            StartCoroutine(ReadDataFile());
+        }
+    }
+
+    private IEnumerator ReadDataFile()
+    {
+        bestScore_ = BinaryDataStream.Read<BestScoreData>(bestScoreKey_);
+        yield return new WaitForEndOfFrame();
+        Debug.Log("Điểm tốt nhất = " + bestScore_.score);
+
+    }   
     private void Start()
     {
         currentScores_ = 0;
+        newBestScore = false;
         UpdateScores();
     }
 
     private void OnEnable()
     {
         GameEvents.AddScores += AddScores;
+        GameEvents.GameOver += SaveBestScore;
 
     }
 
     private void OnDisable()
     {
         GameEvents.AddScores -= AddScores;
+        GameEvents.GameOver -= SaveBestScore;
         
     }
 
     private void AddScores(int scores)
     {
         currentScores_ += scores;
+        if (currentScores_ > bestScore_.score)
+        {
+            newBestScore = true;
+            bestScore_.score = currentScores_;
+            
+        }
         UpdateScores(); 
     }   
     
     private void UpdateScores()
     { 
         scoreText.text = currentScores_.ToString();
+    }
+    
+    public void SaveBestScore(bool newBestScore)
+    {
+        BinaryDataStream.Save<BestScoreData>(bestScore_, bestScoreKey_);
     }    
 }
---
+++ Assets/Script/Ultility/BinaryDataStream.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+
+public class BinaryDataStream 
+{
+    public static void Save<T>(T serializedObject, string fileName)
+    {
+        string path = Application.persistentDataPath + "/save/";
+
+        Directory.CreateDirectory(path);
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream fileStream = new FileStream(path + fileName + ".data", FileMode.Create);
+        Debug.Log("Đang lưu file tại: " + path);
+
+        try
+        {
+            formatter.Serialize(fileStream, serializedObject);
+
+        }
+        catch (SerializationException e) 
+        { 
+            Debug.Log("Lưu file: "+ e.Message);
+        }
+        finally
+        {
+            fileStream.Close();
+        }
+    }   
+    
+    public static bool Exist(string fileName)
+    {
+        string path = Application.persistentDataPath + "/save/";
+        string fullFileName = fileName + ".data";
+        return File.Exists(path + fullFileName);
+    }
+
+    public static T Read<T>(string fileName)
+    {
+        string path = Application.persistentDataPath + "/save/";
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream fileStream = new FileStream(path + fileName + ".data", FileMode.Open);
+        T returnType = default(T);
+
+        try
+        {
+            returnType = (T)formatter.Deserialize(fileStream);
+        }
+        catch (SerializationException e)
+        {
+            Debug.Log("Đọc thất bại. Lỗi: " + e.Message);
+        }
+        finally
+        {
+            fileStream.Close();
+        }
+
+        return returnType;
+    }
+}
