b0d12f16b71d67457543cce579126e170ba6a8d0
NgDinhThi <dinhthit25@gmail.com>
2025-08-06 23:47:51 +0700

    thêm ui và tí hệ thống
    thêm hệ thống âm thanh và thêm ui cho setting screen nó đã thêm phần chỉnh âm lượng âm thanh

---
+++ Assets/Script/Sound/AudioManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public static AudioManager instance;
+    public Sound[] sounds;
+
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        // Tạo AudioSource cho từng sound
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.loop = s.loop;
+            s.source.pitch = s.pitch <= 0 ? 1f : s.pitch;
+            s.source.ignoreListenerPause = true;
+            s.source.volume = s.volume;
+        }
+
+      
+    }
+
+    private void Update()
+    {
+        foreach (Sound s in sounds)
+        {
+            if (s.source != null)
+                s.source.volume = s.volume;
+        }
+    }
+
+    private void Start()
+    {
+        Play("OpenSound");
+    }
+
+
+    public void Play(string name)
+    {
+       
+        Sound s = Array.Find(sounds, sound => sound.name == name);     
+        s.source.volume = s.volume;
+        s.source.Play();
+    }
+
+
+}
---
+++ Assets/Script/UI/AudioSettingsUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioSettingsUI : MonoBehaviour
+{
+    public Slider volumeSlider;
+
+    private void Start()
+    {
+        // Nếu muốn mặc định 0.5
+        volumeSlider.value = 0.5f;
+
+        // Gán giá trị volume ban đầu cho tất cả sound
+        foreach (Sound s in AudioManager.instance.sounds)
+        {
+            s.volume = volumeSlider.value;
+        }
+
+        // Gán sự kiện khi kéo slider
+        volumeSlider.onValueChanged.AddListener(HandleVolumeChanged);
+    }
+
+    private void HandleVolumeChanged(float value)
+    {
+        // Cập nhật volume cho tất cả sound
+        foreach (Sound s in AudioManager.instance.sounds)
+        {
+            s.volume = value;
+        }
+    }
+}
